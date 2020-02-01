fd67b6d34bf0f955b8b6cf48db15f54b89a1a0ee
Danny van Duist <dvduist@hotmail.nl>
2020-02-01 01:39:55 +0100

    A component to make a character controller move with a (specified) joystick now exists. And created some folder structure.

---
+++ Assets/zExternalCode/VDFramework/Extensions/UnityExtensions/GameObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VDUnityFramework.UnityExtensions
+{
+	public static class GameObjectExtensions
+	{
+		/// <summary>
+		/// Ensures that the specified component is on this <see cref="GameObject"/>.
+		/// </summary>
+		public static TComponent EnsureComponent<TComponent>(this GameObject gameObject) where TComponent: Component
+		{
+			return gameObject.TryGetComponent(out TComponent component) ? component : gameObject.AddComponent<TComponent>();
+		}
+	}
+}
