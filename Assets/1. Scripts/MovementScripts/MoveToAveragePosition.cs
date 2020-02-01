00d4536f97cab8f0f23beac08eaa16f62a4045ae
Danny van Duist <dvduist@hotmail.nl>
2020-02-01 11:22:24 +0100

    Created a new scene.

---
+++ Assets/1. Scripts/MovementScripts/MoveToAveragePosition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using VDUnityFramework.BaseClasses;
+
+namespace MovementScripts
+{
+	public class MoveToAveragePosition : BetterMonoBehaviour
+	{
+		[SerializeField] private Vector3 offset = Vector3.zero;
+		[SerializeField] private string tagToSearchFor = "Player";
+
+		[Header("Will search for the tag if empty.")]
+		[SerializeField] private GameObject[] objects = new GameObject[0];
+
+		private void Start()
+		{
+			if (objects.Length == 0)
+			{
+				objects = GameObject.FindGameObjectsWithTag(tagToSearchFor);
+			}
+		}
+
+		private void LateUpdate()
+		{
+			CachedTransform.position = FindAveragePosition() + offset;
+		}
+
+		private Vector3 FindAveragePosition()
+		{
+			return objects.Aggregate(Vector3.zero, (current, gameobject) => current + gameobject.transform.position) /
+				   objects.Length;
+		}
+	}
+}
---
+++ Assets/1. Scripts/MovementScripts/ZoomCameraToFitAll.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using VDUnityFramework.BaseClasses;
+using VDUnityFramework.UnityExtensions;
+
+namespace MovementScripts
+{
+	public class ZoomCameraToFitAll : BetterMonoBehaviour
+	{
+		[SerializeField] private Vector3 offset = Vector3.zero;
+		[SerializeField] private string tagToSearchFor = "Player";
+
+		[Header("Will search for the tag if empty.")] [SerializeField]
+		private GameObject[] objects = new GameObject[0];
+
+		private float aspectRatio;
+		private float tanFOV;
+
+		private Camera mainCamera;
+
+		private void Awake()
+		{
+			mainCamera = Camera.main;
+
+			aspectRatio = Screen.height / (float) Screen.width;
+			tanFOV = Mathf.Tan(Mathf.Deg2Rad * mainCamera.fieldOfView / 2.0f);
+		}
+
+		private void Start()
+		{
+			if (objects.Length == 0)
+			{
+				objects = GameObject.FindGameObjectsWithTag(tagToSearchFor);
+			}
+		}
+
+		private void LateUpdate()
+		{
+			GameObject[] objects = FindFurthestObjects();
+		}
+
+		private GameObject[] FindFurthestObjects()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
