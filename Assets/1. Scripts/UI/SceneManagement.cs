f2a1eb59852ab1bad493e767961a5d6a15f283dd
Flippo13 <felixdenatris@hotmail.nl>
2020-02-02 05:51:16 +0100

    Added Menu Interaction

---
+++ Assets/1. Scripts/UI/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
