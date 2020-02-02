5bdbf303003263048be87fe8d6196da697cb73ed
Danny van Duist <dvduist@hotmail.nl>
2020-02-02 12:48:37 +0100

    Internet script helped solve the problem. It did the polygon check.

---
+++ Assets/zExternalCode/Internet/Poly.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Internet
+{
+	public static class Poly
+	{
+		public static bool ContainsPoint(Vector2[] polyPoints, Vector2 p)
+		{
+			int j = polyPoints.Length - 1;
+			bool inside = false;
+			
+			for (int i = 0; i < polyPoints.Length; j = i++)
+			{
+				Vector2 pi = polyPoints[i];
+				Vector2 pj = polyPoints[j];
+				if (((pi.y <= p.y && p.y < pj.y) || (pj.y <= p.y && p.y < pi.y)) &&
+					(p.x < (pj.x - pi.x) * (p.y - pi.y) / (pj.y - pi.y) + pi.x))
+					inside = !inside;
+			}
+			return inside;
+		}
+	}
+}
