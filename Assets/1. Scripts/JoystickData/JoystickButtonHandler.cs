981b70d60cb7f26b740e7595f8558f24f990a0a3
Danny van Duist <dvduist@hotmail.nl>
2020-02-02 13:42:20 +0100

    Use JoystickButtonHandler.IsShoulderButtonPressed(uint)

---
+++ Assets/1. Scripts/JoystickData/JoystickButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace JoystickData
+{
+	public static class JoystickButtonHandler
+	{
+		public static bool IsShoulderButtonPressed(uint joystickNumber)
+		{
+			return Input.GetAxis($"RShoulderButton_Joystick_{joystickNumber}") > 0;
+		}
+	}
+}
