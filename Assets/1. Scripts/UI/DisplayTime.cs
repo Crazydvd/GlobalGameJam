ef8a547647a47e06ed050d6384f5ce8db52b0a1c
Flippo13 <felixdenatris@hotmail.nl>
2020-02-02 09:39:20 +0100

    Shoulder Button Input

---
+++ Assets/1. Scripts/UI/DisplayTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Timer;
+using UnityEngine.UI; 
+using UnityEngine;
+
+public class DisplayTime : MonoBehaviour
+{
+    public Text displayTime;
+    public GameTimer gameTime; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayTime.text = Mathf.RoundToInt(gameTime.GetSecondsLeft()).ToString();
+    }
+}
