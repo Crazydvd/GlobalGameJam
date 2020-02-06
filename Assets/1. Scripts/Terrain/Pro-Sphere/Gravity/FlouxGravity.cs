743a6c79f89afab27f01dc699b48960d615cb6a6
Vaster <vasil.grancharov1996@gmail.com>
2020-02-06 11:41:31 +0100

    gravity uncomplete, Materials update, no water!!!!!

---
+++ Assets/1. Scripts/Terrain/Pro-Sphere/Gravity/FlouxGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlouxGravity : MonoBehaviour
+{
+    public GravityAttractor attractor;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        attractor.Attract(transform);
+    }
+}
