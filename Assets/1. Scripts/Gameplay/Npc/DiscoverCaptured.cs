f8964febd9c4266114a8589022466d49866ffb47
Danny van Duist <dvduist@hotmail.nl>
2020-02-06 22:46:22 +0100

    Updated the folder structure.

---
+++ Assets/1. Scripts/Gameplay/Npc/DiscoverCaptured.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Events.GameplayEvents;
+using Fence;
+using UnityEngine;
+using VDUnityFramework.BaseClasses;
+using VDUnityFramework.EventSystem;
+using Internet;
+
+namespace Gameplay
+{
+	public class DiscoverCaptured : BetterMonoBehaviour
+	{
+		private void Awake()
+		{
+			EventManager.Instance.AddListener<FenceCompletedEvent>(OnFenceCompleted);
+		}
+
+		private void OnDestroy()
+		{
+			if (EventManager.IsInitialized)
+			{
+				EventManager.Instance.RemoveListener<FenceCompletedEvent>(OnFenceCompleted);
+			}
+		}
+
+		private void OnFenceCompleted(FenceCompletedEvent fenceCompletedEvent)
+		{
+			if (IsTrappedInFence(fenceCompletedEvent.Origin))
+			{
+				EventManager.Instance.RaiseEvent(new SheepCapturedEvent());
+
+				Destroy(gameObject);
+			}
+		}
+
+		private bool IsTrappedInFence(FenceCorner origin)
+		{
+			List<FenceCorner> corners = new List<FenceCorner>();
+			FenceCorner currentCorner = origin;
+			corners.Add(origin);
+
+			do
+			{
+				currentCorner = currentCorner.Child;
+				corners.Add(currentCorner);
+
+			} while (currentCorner != origin);
+
+			List<Vector2> polygonVertices = corners.Select(fenceCorner => new Vector2(fenceCorner.CachedTransform.position.x, fenceCorner.CachedTransform.position.z)).ToList();
+			Vector2 currentPos = new Vector2(CachedTransform.position.x, CachedTransform.position.z);
+
+			return Poly.ContainsPoint(polygonVertices.ToArray(), currentPos);
+		}
+	}
+}
---
+++ Assets/1. Scripts/Gameplay/Player2/AttractToggler.cs
@@ -0,0 +1,32 @@
+﻿using Enums;
+using JoystickData;
+using VDUnityFramework.BaseClasses;
+using VDUnityFramework.EventSystem;
+
+namespace Gameplay
+{
+	public class AttractToggler : BetterMonoBehaviour
+	{
+		private uint joystickNumber;
+
+		private bool attractEnabled = false;
+
+		private AttractScript attractScript;
+				
+		private void Awake()
+		{
+			attractScript = GetComponent<AttractScript>();
+			joystickNumber = GetComponent<JoystickNumber>();
+		}
+
+		private void Update()
+		{
+			if (JoystickButtonHandler.Instance.GetButtonDown(joystickNumber, JoystickButton.RightShoulderButton))
+			{
+				attractEnabled = !attractEnabled;
+
+				EventManager.Instance.RaiseEvent(new ToggleAttractEvent(attractEnabled, attractScript));
+			}
+		}
+	}
+}
